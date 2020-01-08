49ed7131e56d139d5247540303ddf8d1cb456d44
parkjungkwan <pakjkwan@gmail.com>
2020-01-08 11:24:28 +0900

    11:24

--- java/com/example/application/MemberDetail.java
+++ java/com/example/application/MemberDetail.java
@@ -2,10 +2,16 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.function.Supplier;
 
 public class MemberDetail extends AppCompatActivity {
 
@@ -15,7 +21,52 @@ public class MemberDetail extends AppCompatActivity {
         setContentView(R.layout.member_detail);
         final Context _this = MemberDetail.this;
         Intent intent = this.getIntent();
-        String seq = intent.getExtras().getString("seq");
-        Toast.makeText(_this, "넘어온 값"+seq, Toast.LENGTH_LONG).show();
+        final ItemDetail query = new ItemDetail(_this);
+        query.seq = Integer.parseInt(intent.getExtras().getString("seq"))+1;
+        Main.Member member = new Supplier<Main.Member>() {
+            @Override
+            public Main.Member get() {
+                return query.get();
+            }
+        }.get();
+
+        Toast.makeText(_this, "회원이름 "+member.name, Toast.LENGTH_LONG).show();
+
+    }
+    private class MemberDetailQuery extends Main.QueryFactory{
+        SQLiteOpenHelper helper;
+        public MemberDetailQuery(Context _this) {
+            super(_this);
+            helper = new Main.SQLiteHelper(_this);
+        }
+
+        @Override
+        public SQLiteDatabase getDatabase() {
+            return helper.getReadableDatabase();
+        }
+    }
+    private class ItemDetail extends MemberDetailQuery{
+        int seq;
+        public ItemDetail(Context _this) {
+            super(_this);
+        }
+        public Main.Member get(){
+            Main.Member member = null;
+            Toast.makeText(_this, "쿼리에 들어갈 seq "+seq, Toast.LENGTH_LONG).show();
+            Cursor cursor = getDatabase()
+                    .rawQuery(String.format("SELECT * FROM %s WHERE %s LIKE '%s'",
+                            Main.MEMBERS, Main.SEQ, seq),null);
+            if(cursor != null && cursor.moveToNext()){
+                member = new Main.Member();
+                member.seq = Integer.parseInt(cursor.getString(cursor.getColumnIndex(Main.SEQ)));
+                member.name = cursor.getString(cursor.getColumnIndex(Main.NAME));
+                member.passwd = cursor.getString(cursor.getColumnIndex(Main.PASSWD));
+                member.email = cursor.getString(cursor.getColumnIndex(Main.EMAIL));
+                member.phone = cursor.getString(cursor.getColumnIndex(Main.PHONE));
+                member.addr = cursor.getString(cursor.getColumnIndex(Main.ADDR));
+                member.photo = cursor.getString(cursor.getColumnIndex(Main.PHOTO));
+            }
+            return member;
+        }
     }
 }
